93c4948abb382451dad22dbda41ae5e6a3742ee6
Rafferty <rcruikshank2744@student.pps.net>
2025-03-04 11:39:45 -0800

    Sniper

--- Assets/Weapons/GunDamage.cs
+++ Assets/Weapons/GunDamage.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
+
 public class GunDamage : MonoBehaviour
 {
     public PlayerInput playerInput;
@@ -21,63 +22,91 @@ public class GunDamage : MonoBehaviour
     public float FireCoolDown;
     public bool Automatic;
     public float CurrentCoolDown;
-    
-      private void Awake()
+    public bool burst;
+
+    // New variables for burst fire
+    public int BurstCount = 3; // Number of rounds in the burst
+    public float BurstInterval = 0.1f; // Time between each burst shot
+    private bool isBursting = false;
+
+    private void Awake()
     {
         // Get the Fire action from the input asset
         fireAction = playerInput.actions["Fire"];
         PlayerCamera = Camera.main.transform;
-
-        
     }
 
-   private void Update()
+    private void Update()
     {
-        // Check if the fire action is triggered
-        if (fireAction.IsPressed()) // Use IsPressed for continuous fire when holding
+        if (fireAction.IsPressed()) 
         {
             if (CurrentCoolDown <= 0f)
             {
-                Shoot(); // Trigger the shooting event
-                CurrentCoolDown = FireCoolDown; // Reset cooldown after shooting
+                if (burst == true){
+                if (!isBursting) 
+                {
+                    StartCoroutine(BurstFire()); // Start burst fire when not already bursting
+                }
+                }
+                else {
+                    Shoot();
+               CurrentCoolDown = FireCoolDown;
+
+                }
             }
         }
 
         // Decrease cooldown each frame
         CurrentCoolDown -= Time.deltaTime;
     }
-  public void Shoot(){
-   CurrentCoolDown = FireCoolDown;
-   MuzzleFlash.Play();
-   RecoilObject.recoil += recoilamount;
-   Ray gunRay = new Ray(bulletSpawnPoint.position, bulletSpawnPoint.forward);
-   Debug.Log("Shot");
-   //Debug.DrawRay(PlayerCamera.position, PlayerCamera.forward);
-   // Check if the ray hits anything
-   if (Physics.Raycast(gunRay, out RaycastHit hitInfo, BulletRange))
-   {
-       // Spawn the bullet trail
-       TrailRenderer trail = Instantiate(BulletTrail, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
-       StartCoroutine(SpawnTrail(trail, hitInfo.point)); // Use the hit point
-       // Check if the hit object has an Entity component
-       if (hitInfo.collider.gameObject.TryGetComponent(out Entity enemy))
-       {
-           enemy.Health -= Damage;
-           //Debug.Log("Hit entity");
-       }
-   }
-   else
-   {
-       // If no object is hit, calculate the endpoint based on the bullet range
-       Vector3 missPoint = gunRay.origin + gunRay.direction * BulletRange;
 
+    private IEnumerator BurstFire()
+    {
+        isBursting = true;
 
-       // Spawn the bullet trail
-       TrailRenderer trail = Instantiate(BulletTrail, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
-       StartCoroutine(SpawnTrail(trail, missPoint)); // Use the calculated miss point
-   }
-}
-private IEnumerator SpawnTrail(TrailRenderer Trail, Vector3 targetPoint)
+        for (int i = 0; i < BurstCount; i++)
+        {
+            Shoot();  // Shoot a single shot
+            yield return new WaitForSeconds(BurstInterval);  // Wait before firing the next shot
+        }
+
+        // Reset cooldown after burst fire
+        CurrentCoolDown = FireCoolDown;
+        isBursting = false;
+    }
+
+    public void Shoot()
+    {
+        MuzzleFlash.Play();
+        RecoilObject.recoil += recoilamount;
+        Ray gunRay = new Ray(bulletSpawnPoint.position, bulletSpawnPoint.forward);
+        Debug.Log("Shot");
+
+        // Check if the ray hits anything
+        if (Physics.Raycast(gunRay, out RaycastHit hitInfo, BulletRange))
+        {
+            // Spawn the bullet trail
+            TrailRenderer trail = Instantiate(BulletTrail, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+            StartCoroutine(SpawnTrail(trail, hitInfo.point)); // Use the hit point
+            // Check if the hit object has an Entity component
+            if (hitInfo.collider.gameObject.TryGetComponent(out Entity enemy))
+            {
+                enemy.Health -= Damage;
+                // Debug.Log("Hit entity");
+            }
+        }
+        else
+        {
+            // If no object is hit, calculate the endpoint based on the bullet range
+            Vector3 missPoint = gunRay.origin + gunRay.direction * BulletRange;
+
+            // Spawn the bullet trail
+            TrailRenderer trail = Instantiate(BulletTrail, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+            StartCoroutine(SpawnTrail(trail, missPoint)); // Use the calculated miss point
+        }
+    }
+
+    private IEnumerator SpawnTrail(TrailRenderer Trail, Vector3 targetPoint)
     {
         float distance = Vector3.Distance(Trail.transform.position, targetPoint);
         float startingDistance = distance;
