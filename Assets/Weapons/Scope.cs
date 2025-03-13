e2aef943f91b92fe0b3509f35c3e0b32c0bd681e
Rafferty <rcruikshank2744@student.pps.net>
2025-03-13 12:03:11 -0700

    animating the sniper

--- Assets/Weapons/Scope.cs
+++ Assets/Weapons/Scope.cs
@@ -7,17 +7,34 @@ public class Scope : MonoBehaviour
 {
     public Animator animator;
     public PlayerInput playerInput;
+
+    public GameObject scopeOverlay;
     private bool IsScoped = false;
     private InputAction fireAction;
     void Awake()
     {
         fireAction = playerInput.actions["Scope"];
+        scopeOverlay.SetActive(false);
     }
     void Update(){
          if (fireAction.WasPressedThisFrame()) 
         {
             IsScoped = !IsScoped;
             animator.SetBool("Scoped", IsScoped);
+            scopeOverlay.SetActive(IsScoped);
+            if (IsScoped){
+            StartCoroutine(OnScoped());
+            }
+            else{
+            Unscoped();
+            }
     }
+}
+void Unscoped(){
+scopeOverlay.SetActive(false);
+}
+IEnumerator OnScoped(){
+    yield return new WaitForSeconds(.15f);
+    scopeOverlay.SetActive(true);
 }
 }
