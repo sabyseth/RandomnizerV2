405d10395e22d594dd97c7410e724ff8acbb5b9b
Rafferty <rcruikshank2744@student.pps.net>
2025-03-17 11:41:31 -0700

    working sniper and scope script + quicksoping

--- Assets/Weapons/Scope.cs
+++ Assets/Weapons/Scope.cs
@@ -7,14 +7,19 @@ public class Scope : MonoBehaviour
 {
     public Animator animator;
     public PlayerInput playerInput;
-
+    public Camera mainCamera;
     public GameObject scopeOverlay;
+    public GameObject weaponCamera;
+    public float scopedFOV = 15f;
+    private float normalFOV;
     private bool IsScoped = false;
     private InputAction fireAction;
+    private InputAction shootAction;
     void Awake()
     {
         fireAction = playerInput.actions["Scope"];
         scopeOverlay.SetActive(false);
+        shootAction = playerInput.actions["Fire"];
     }
     void Update(){
          if (fireAction.WasPressedThisFrame()) 
@@ -29,12 +34,22 @@ public class Scope : MonoBehaviour
             Unscoped();
             }
     }
+          if (shootAction.WasPressedThisFrame() && IsScoped)
+        {
+            Unscoped();
+            animator.SetBool("Scoped", false);
+        }
 }
 void Unscoped(){
 scopeOverlay.SetActive(false);
+weaponCamera.SetActive(true);
+mainCamera.fieldOfView = normalFOV;
 }
 IEnumerator OnScoped(){
-    yield return new WaitForSeconds(.15f);
-    scopeOverlay.SetActive(true);
+        normalFOV = mainCamera.fieldOfView; 
+        mainCamera.fieldOfView = scopedFOV; 
+        scopeOverlay.SetActive(true);
+        yield return new WaitForSeconds(0.075f); 
+        weaponCamera.SetActive(false);
 }
 }
